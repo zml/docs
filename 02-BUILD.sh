7f9975da871134efb244bcd58a704311390bb724
Rene Schallner <rene@renerocks.ai>
2024-09-11 02:12:53 +0200

    try again

--- 02-BUILD.sh
+++ 02-BUILD.sh
@@ -1,8 +1,10 @@
 cd zml
+echo "Starting Bazel build..."
 bazel build //zml:docs
 cd ..
 # we reference these outputs in build.zig
-# cp ./zml/bazel-bin/zml/docs.docs/sources.tar .
-# cp ./zml/bazel-bin/zml/docs.docs/sources.tar .
+# - ./zml/bazel-bin/zml/docs.docs/sources.tar
+# - ./zml/bazel-bin/zml/docs.docs/main.wasm
 cd WORKSPACE
+echo "Starting Zine build..."
 zig build $@
