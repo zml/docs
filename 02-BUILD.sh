b27feb15936f1e217df5ca1df8a9240bf856cb3c
Guillaume Wenzek <gwenzek@users.noreply.github.com>
2024-09-13 13:47:53 +0200

    use tar instead of bazel

--- 02-BUILD.sh
+++ 02-BUILD.sh
@@ -1,8 +1,12 @@
 cd zml
-echo "Starting Bazel build..."
-bazel build //zml:sources
+# We don't use bazel build //zml:sources
+# because it pulls too many files.
+# Some could be useful like sentencepiece.pb.zig,
+# but they aren't properly put in places where the wasm code expects them,
+# resulting in broken links + polluting the search bar.
+tar -cf sources.tar zml/*.zig zml/**/*.zig
 cd ..
-cp ./zml/bazel-bin/zml/sources.tar WORKSPACE/
+cp -f ./zml/sources.tar WORKSPACE/
 
 cd WORKSPACE
 echo "Starting Zine build..."
