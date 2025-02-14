f5a2c5163b491930785a95fad22e53c9463cbda0
Rene Schallner <rene@zml.ai>
2025-02-14 13:44:00 +0100

    wip

--- tools/processor.zig
+++ tools/processor.zig
@@ -1,4 +1,34 @@
 const std = @import("std");
+const shell = @import("shell.zig");
+
+const Action = union(enum) {
+    CreateDir: []const u8,
+    TranslateLink: struct {
+        source_file: []const u8,
+        original: []const u8,
+        destination: []const u8,
+    },
+    MergeIntoSmd: struct {
+        smd_dest: []const u8,
+        smd_source: []const u8,
+        md_source: []const u8,
+    },
+    SplitSmd: struct {
+        source_file: []const u8,
+        smd_dest: []const u8,
+        md_dest: []const u8,
+    },
+    ProcessingFile: []const u8,
+    IgnoreFile: struct {
+        source_file: []const u8,
+        reason: []const u8,
+    },
+    TranslateImage: struct {
+        source_file: []const u8,
+        original: []const u8,
+        destination: []const u8,
+    },
+};
 
 pub fn main() !void {
     std.debug.print("All your codebase\n", .{});
